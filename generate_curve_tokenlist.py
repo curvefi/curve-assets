05f3b9634d6e41b7f65b6a98201d801cfcf7c83d
bout3fiddy <11488427+bout3fiddy@users.noreply.github.com>
2024-09-14 15:32:14 +0200

    upload to ghpages

--- generate_curve_tokenlist.py
+++ generate_curve_tokenlist.py
@@ -1,6 +1,6 @@
 # flake8: noqa: E402
 import os
-from typing import List, Optional
+from typing import Dict, List, Optional
 
 from rich.console import Console
 from rich.theme import Theme
@@ -22,50 +22,6 @@ custom_theme = Theme(
 
 # Initialize Rich console with our custom theme
 console = Console(theme=custom_theme)
-
-
-def main(networks_to_ignore: Optional[List[str]] = None) -> None:
-    console.print("[info]Starting token list generation...[/info]")
-
-    try:
-        existing_tokenlist = load_json("curve_tokenlist.json")
-        existing_tokens = existing_tokenlist.get("tokens", [])
-
-        networks, tokens_in_folder, tokens_to_add = scan_images_folder()
-        if networks_to_ignore:
-            networks = [net for net in networks if net not in networks_to_ignore]
-
-        display_summary(networks, tokens_in_folder, tokens_to_add)
-
-        all_failed_tokens = {}
-        processed_tokens = []
-        all_skipped_tokens = []
-
-        for network_name in NETWORKS:
-            network_tokens, skipped_tokens = process_network(network_name, existing_tokens, all_failed_tokens)
-            processed_tokens.extend(network_tokens)
-            all_skipped_tokens.extend(skipped_tokens)
-
-            # Ensure the native token is present in the list
-            ensure_native_token_in_list(existing_tokenlist, network_name)
-
-            # Update the tokenlist after each network
-            updated_tokenlist = update_tokenlist(processed_tokens, all_skipped_tokens, existing_tokenlist)
-            save_json(updated_tokenlist, "curve_tokenlist.json")
-
-        # Check if there are any failed tokens
-        if all_failed_tokens:
-            console.print(
-                "[yellow]Some tokens failed to return data or validate. Check failed_tokens_report.json for details.[/yellow]"
-            )
-            save_json(all_failed_tokens, "failed_tokens_report.json")
-        else:
-            console.print("[green]All tokens processed successfully![/green]")
-
-        console.print("[green]Token list generation completed![/green]")
-    except Exception as e:
-        console.print(f"[error]An error occurred: {str(e)}[/error]")
-        raise
 
 
 def ensure_native_token_in_list(tokenlist, network_name):
@@ -96,5 +52,53 @@ def ensure_native_token_in_list(tokenlist, network_name):
     tokenlist["tokenMap"][token_key] = native_token_info
 
 
+def generate_tokenlist(networks_to_ignore: Optional[List[str]] = None, save_local_copy: bool = False) -> Dict:
+    console.print("[info]Starting token list generation...[/info]")
+
+    try:
+        existing_tokenlist = load_json("curve_tokenlist.json")
+        existing_tokens = existing_tokenlist.get("tokens", [])
+
+        networks, tokens_in_folder, tokens_to_add = scan_images_folder()
+        if networks_to_ignore:
+            networks = [net for net in networks if net not in networks_to_ignore]
+
+        display_summary(networks, tokens_in_folder, tokens_to_add)
+
+        all_failed_tokens = {}
+        processed_tokens = []
+        all_skipped_tokens = []
+
+        for network_name in NETWORKS:
+            network_tokens, skipped_tokens = process_network(network_name, existing_tokens, all_failed_tokens)
+            processed_tokens.extend(network_tokens)
+            all_skipped_tokens.extend(skipped_tokens)
+
+            # Ensure the native token is present in the list
+            ensure_native_token_in_list(existing_tokenlist, network_name)
+
+        # Update the tokenlist after processing all networks
+        updated_tokenlist = update_tokenlist(processed_tokens, all_skipped_tokens, existing_tokenlist)
+
+        if save_local_copy:
+            save_json(updated_tokenlist, "curve_tokenlist.json")
+            console.print("[green]Tokenlist saved locally.[/green]")
+
+        # Check if there are any failed tokens
+        if all_failed_tokens:
+            console.print(
+                "[yellow]Some tokens failed to return data or validate. Check failed_tokens_report.json for details.[/yellow]"
+            )
+            save_json(all_failed_tokens, "failed_tokens_report.json")
+        else:
+            console.print("[green]All tokens processed successfully![/green]")
+
+        console.print("[green]Token list generation completed![/green]")
+        return updated_tokenlist
+    except Exception as e:
+        console.print(f"[error]An error occurred: {str(e)}[/error]")
+        raise
+
+
 if __name__ == "__main__":
-    main(networks_to_ignore=["assets-harmony"])
+    generate_tokenlist(networks_to_ignore=["assets-harmony"], save_local_copy=True)
